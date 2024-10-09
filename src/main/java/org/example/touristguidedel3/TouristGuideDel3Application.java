36f7f810fd917dd8054fe736b648974355bdd553
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-10-09 13:45:28 +0200

    Initial commit

---
+++ src/main/java/org/example/touristguidedel3/TouristGuideDel3Application.java
@@ -0,0 +1,13 @@
+package org.example.touristguidedel3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideDel3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristGuideDel3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/touristguidedel3/TouristGuideDel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.touristguidedel3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideDel3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
