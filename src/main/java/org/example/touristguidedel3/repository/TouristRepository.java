43d0255b23befbb13b52b783e2639ed7c0605b87
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-10-09 14:44:45 +0200

    App properties

--- src/main/java/org/example/touristguidedel3/repository/TouristRepository.java
+++ src/main/java/org/example/touristguidedel3/repository/TouristRepository.java
@@ -1,4 +1,14 @@
 package org.example.touristguidedel3.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TouristRepository {
+
+    public String url ="url;
+    public String user = "username";
+    public String password ="password";
+
+
 }
