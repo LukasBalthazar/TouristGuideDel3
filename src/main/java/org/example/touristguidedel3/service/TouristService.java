48aa90a54d4aa662f772c467eea4da5d074f4e8d
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-10-19 20:06:26 +0200

    Service framework

--- src/main/java/org/example/touristguidedel3/service/TouristService.java
+++ src/main/java/org/example/touristguidedel3/service/TouristService.java
@@ -1,4 +1,42 @@
 package org.example.touristguidedel3.service;
 
+import org.example.touristguidedel3.model.TouristAttraction;
+import org.example.touristguidedel3.repository.TouristRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TouristService {
+
+    @Autowired
+    private TouristRepository touristRepository;
+
+    public String addTouristAttraction(TouristAttraction touristAttraction) {
+        touristRepository.addTouristAttraction(touristAttraction);
+        return "Tourist Attraction added successfully!";
+    }
+
+    // Get all tourist attractions
+    public List<TouristAttraction> getAllAttractions() {
+        return touristRepository.getAllAttractions();
+    }
+
+    // Get tourist attractions by tag
+    public List<TouristAttraction> getAttractionsByTag(String tag) {
+        return touristRepository.getSpecificAttractionsByTags(tag);
+    }
+
+    // Update a tourist attraction
+    public String updateTouristAttraction(int attractionNo) {
+        touristRepository.updateTouristAttraction(attractionNo);
+        return "Tourist Attraction updated successfully!";
+    }
+
+    // Delete a tourist attraction
+    public String deleteTouristAttraction(int attractionNo) {
+        touristRepository.deleteTouristAttraction();
+        return "Tourist Attraction deleted successfully!";
+    }
 }
