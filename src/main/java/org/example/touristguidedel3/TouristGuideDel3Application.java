1f879e009c89253306e292ec08f8eed9dfdee778
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-10-20 22:59:58 +0200

    Cleanup unused code

--- src/main/java/org/example/touristguidedel3/TouristGuideDel3Application.java
+++ src/main/java/org/example/touristguidedel3/TouristGuideDel3Application.java
@@ -22,34 +22,8 @@ public class TouristGuideDel3Application {
             System.out.println(attraction);
         }
 
-        // Uncomment to test the database connection
-        // testDatabaseConnection();
     }
 
-    // Uncomment this method to test database connection
-    /*
-    private static void testDatabaseConnection() {
-        // Database connection configurations
-        String url = System.getenv("DB_url");
-        String user = System.getenv("username");
-        String password = System.getenv("password");
-
-        System.out.println("DB URL: " + url);
-        System.out.println("Username: " + user);
-        System.out.println("Password: " + password);
-
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            if (connection != null) {
-                System.out.println("Successfully connected to the database!");
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection failed!");
-            e.printStackTrace();
-        }
-    }
-    */
 }
 
 
